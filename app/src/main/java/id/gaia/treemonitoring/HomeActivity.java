91fd568a9a95f77a31dc30bc6d42459484d1f73e
Muhammad Hidayatullah <dayat.gerrard@gmail.com>
2018-03-27 10:57:40 +0700

    new upload

---
+++ app/src/main/java/id/gaia/treemonitoring/HomeActivity.java
@@ -0,0 +1,13 @@
+package id.gaia.treemonitoring;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
