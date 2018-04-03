438461cb215a03c442bd4254ed3ad145f3d23572
Muhammad Hidayatullah <dayat.gerrard@gmail.com>
2018-04-03 10:10:51 +0700

    update

--- app/src/main/java/id/gaia/treemonitoring/HomeActivity.java
+++ app/src/main/java/id/gaia/treemonitoring/HomeActivity.java
@@ -2,6 +2,9 @@ package id.gaia.treemonitoring;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.setting_menu, menu);
+        return true;
+    }
+
+
+
+
 }
